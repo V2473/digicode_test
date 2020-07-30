e1a7b619123731605581e11b4db01ae78a7f02c7
Vovasef <vovasef@gmail.com>
2020-07-30 07:40:47 +0300

    MVC and Classes

--- src/main.js
+++ src/main.js
@@ -1,178 +1,341 @@
+/* eslint-disable max-len */
+/* eslint-disable no-undef */
 import * as PIXI from 'pixi.js';
 
+// CONTROLLER
+
+jQuery(document).ready(function() {
+  // This button will increment the value
+  $('.qtyplus').click(function(e) {
+    // Stop acting like a button
+    e.preventDefault();
+    // Get the field name
+    fieldName = $(this).attr('field');
+
+    // Get its current value
+    const currentVal = parseInt($('input[name=' + fieldName + ']').val());
+
+    // If is not undefined
+    if (!isNaN(currentVal)) {
+      // Increment
+      $('input[name=' + fieldName + ']').val(currentVal + 1);
+    } else {
+      // Otherwise put a 0 there
+      $('input[name=' + fieldName + ']').val(0);
+    }
+  });
+
+  // This button will decrement the value till 0
+  $('.qtyminus').click(function(e) {
+    // Stop acting like a button
+    e.preventDefault();
+    // Get the field name
+    fieldName = $(this).attr('field');
+
+    // Get its current value
+    const currentVal = parseInt($('input[name=' + fieldName + ']').val());
+
+    // If it isn't undefined or its greater than 0
+    if (!isNaN(currentVal) && currentVal > 0) {
+      // Decrement one
+      $('input[name=' + fieldName + ']').val(currentVal - 1);
+    } else {
+      // Otherwise put a 0 there
+      $('input[name=' + fieldName + ']').val(0);
+    }
+  });
+});
+
+jQuery(document).ready(function() {
+// This button will increment the value
+  $('.qtyplusS').click(function(e) {
+    // Stop acting like a button
+    e.preventDefault();
+    // Get the field name
+    fieldName = $(this).attr('field');
+
+    // Get its current value
+    const currentVal = parseInt($('input[name=' + fieldName + ']').val());
+
+    // If is not undefined
+    if (!isNaN(currentVal)) {
+      // Increment
+      $('input[name=' + fieldName + ']').val(currentVal + 1);
+    } else {
+      // Otherwise put a 0 there
+      $('input[name=' + fieldName + ']').val(0);
+    }
+  });
+
+  // This button will decrement the value till 0
+  $('.qtyminusS').click(function(e) {
+    // Stop acting like a button
+    e.preventDefault();
+    // Get the field name
+    fieldName = $(this).attr('field');
+
+    // Get its current value
+    const currentVal = parseInt($('input[name=' + fieldName + ']').val());
+
+    // If it isn't undefined or its greater than 0
+    if (!isNaN(currentVal) && currentVal > 0) {
+      // Decrement one
+      $('input[name=' + fieldName + ']').val(currentVal - 1);
+    } else {
+      // Otherwise put a 0 there
+      $('input[name=' + fieldName + ']').val(0);
+    }
+  });
+});
+
+// MODEL
+
+// VIEW
+
+const shapesControl = document.querySelector('.shapes-control');
+const gravityControl = document.querySelector('.gravity-control');
 const counterOfShapes = document.querySelector('.number-of-shapes');
+
 const surface = document.querySelector('.surface');
 let surfaceSum = 0;
-const shapesButton = document.querySelector('.qtyS');
-const gravity = document.querySelector('.qty');
 
 const app = new PIXI.Application({
   width: 800,
   height: 500,
 });
 
-counterOfShapes.innerHTML = app.stage.children.length - 1;
+app.renderer.view.addEventListener('mousedown', (e) => {
+  if (e.target.style.cursor === 'inherit') {
+    makeShape(e.pageX - 25, e.pageY - 75);
+  };
+});
+
+counterOfShapes.innerHTML = app.stage.children.length;
 
 document.querySelector('.app').appendChild(app.view);
 
-app.renderer.backgroundColor = 0x000000;
+app.renderer.backgroundColor = 0xffffff;
 app.renderer.view.style.margin = '20px';
 
-const makeShape = (x, y) => {
-  const colorGenerate = () => {
+class Shape extends PIXI.Graphics {
+  constructor(x, y) {
+    super();
+    this.rotation = Math.random() * 360;
+    this.skewX = Math.random() - 0.5;
+    this.scaleX = Math.random() + 0.5;
+    this.scaleY = Math.random() + 0.5;
+    this.x = x;
+    this.y = y;
+    this.size = Math.random() * 20 + 15;
+    this.color = this.colorGenerate();
+    this.intervalGravity = setInterval(this.gravity(), 10);
+    this.name = this.shapeNameGenerate();
+    this.name1 = this.shapeNameGenerateRandom();
+    this.name2 = this.shapeNameGenerateRandom();
+    this.name3 = this.shapeNameGenerateRandom();
+    this.surface = 0;
+
+    this.on('mousedown', () => {
+      this.deleteShape();
+      this.sameColorChange();
+    });
+  }
+
+  sameColorChange() {
+    app.stage.children.forEach(x => {
+      if (x.name === this.name) {
+        x.colorChange();
+      }
+    });
+  }
+
+  colorGenerate() {
     return '0x' + ((1 << 24) * Math.random() | 0).toString(16);
   };
 
-  const shapeAuto = new PIXI.Graphics();
-  const colorAuto = colorGenerate();
+  shapeNameGenerate() {
+    const shapes = [
+      'circle',
+      'ellipse',
+      '3sides',
+      '4sides',
+      '5sides',
+      '6sides',
+      'random',
+    ];
 
-  shapeGenerate(shapeAuto, colorAuto, x, y);
+    return shapes[Math.floor(Math.random() * shapes.length)];
+  }
 
-  const deleteShape = () => {
-    app.stage.removeChild(shapeAuto);
-    surfaceSum -= shapeAuto.height * shapeAuto.width * 0.6;
-    surface.innerHTML = surfaceSum.toFixed(0);
-    shapeAuto.destroy();
+  shapeNameGenerateRandom() {
+    const shapes = [
+      'ellipse',
+      '3sides',
+      '4sides',
+      '5sides',
+      '6sides',
+    ];
+
+    return shapes[Math.floor(Math.random() * shapes.length)];
+  }
+
+  shapeGenerate(shape) {
+    switch (shape) {
+      case ('circle'):
+        this.drawCircle(0, 0, this.size);
+        this.surface = Math.PI * this.size ** 2;
+        break;
+      case ('ellipse'):
+        this.drawCircle(0, 0, this.size);
+        this.surface = Math.PI * this.size ** 2 * this.scaleX * this.scaleY;
+        break;
+      case ('3sides'):
+        this.moveTo(this.size, -this.size);
+        this.lineTo(0, this.size);
+        this.lineTo(-this.size, -this.size);
+        this.surface = ((this.size * 2) ** 2 * Math.sqrt(3)) / 4;
+        break;
+      case ('4sides'):
+        this.drawRect(
+          -this.size / 2,
+          -this.size / 2,
+          this.size,
+          this.size,
+        );
+        this.surface = (this.size * 2) ** 2 * this.scaleX * this.scaleY;
+        break;
+      case ('5sides'):
+        this.drawPolygon(
+          0, this.size,
+          Math.sin(2 * Math.PI / 5) * this.size,
+          Math.cos(2 * Math.PI / 5) * this.size,
+          Math.sin(4 * Math.PI / 5) * this.size,
+          -Math.cos(Math.PI / 5) * this.size,
+          -Math.sin(4 * Math.PI / 5) * this.size,
+          -Math.cos(Math.PI / 5) * this.size,
+          -Math.sin(2 * Math.PI / 5) * this.size,
+          Math.cos(2 * Math.PI / 5) * this.size,
+        );
+        this.surface = 0.25 * (Math.sqrt(5 * (5 + 2 * Math.sqrt(5)))) * (this.size ** 2) * this.scaleX * this.scaleY;
+        break;
+      case ('6sides'):
+        this.drawPolygon(
+          0, this.size / 2,
+          Math.cos(Math.PI / 6) * this.size, this.size / 4,
+          Math.cos(Math.PI / 6) * this.size, -this.size / 4,
+          0, -this.size / 2,
+          -Math.cos(Math.PI / 6) * this.size, -this.size / 4,
+          -Math.cos(Math.PI / 6) * this.size, this.size / 4,
+        );
+        this.surface = (3 * Math.sqrt(3) / 2) * this.size ** 2 * this.scaleX * this.scaleY;
+        break;
+      case ('random'):
+        this.randomGen();
+        break;
+    }
+
+    if (this.name !== 'circle') {
+      this.setTransform(
+        this.x,
+        this.y,
+        this.scaleX,
+        this.scaleY,
+        this.rotation,
+        this.skewX,
+        0,
+        0,
+        0,
+      );
+    }
+  }
+
+  randomGen() {
+    this.shapeGenerate(this.name1);
+    this.endFill();
+    this.beginFill(this.color);
+    this.shapeGenerate(this.name2);
+    this.endFill();
+    this.beginFill(this.color);
+    this.shapeGenerate(this.name3);
+  }
+
+  start() {
     counterOfShapes.innerHTML = app.stage.children.length;
-    clearInterval(intervalAuto);
-  };
 
-  shapeAuto.on('mousedown', () => {
-    deleteShape();
-  });
+    this.beginFill(this.color);
 
-  app.stage.addChild(shapeAuto);
-  counterOfShapes.innerHTML = app.stage.children.length;
+    this.shapeGenerate(this.name);
 
-  const gravityAuto = (element) => {
+    this.endFill();
+    surfaceSum += this.surface;
+    surface.innerHTML = surfaceSum.toFixed(0);
+    this.interactive = true;
+    this.buttonMode = true;
+    app.stage.addChild(this);
+    counterOfShapes.innerHTML = app.stage.children.length;
+  }
+
+  colorChange() {
+    this.color = this.colorGenerate();
+    surfaceSum -= this.surface;
+    surface.innerHTML = surfaceSum.toFixed(0);
+
+    this.start();
+  }
+
+  gravity() {
     let speed = 0;
 
     return () => {
-      speed += gravity.value / 777;
-      element.y += speed;
+      speed += gravityControl.value / 777;
+      this.y += speed;
 
-      if (shapeAuto.y > app.renderer.view.height + shapeAuto.height) {
-        deleteShape();
+      if (this.y > app.renderer.view.height + this.height) {
+        this.deleteShape();
       }
     };
   };
 
-  const intervalAuto = setInterval(gravityAuto(shapeAuto), 10);
+  deleteShape() {
+    app.stage.removeChild(this);
+    surfaceSum -= this.surface;
+    surface.innerHTML = surfaceSum.toFixed(0);
+    clearInterval(this.intervalGravity);
+    counterOfShapes.innerHTML = app.stage.children.length;
+    this.destroy();
+  };
+}
+
+const makeShape = (x, y) => {
+  const shape = new Shape(x, y);
+
+  shape.start();
 };
 
-const shapeGenerate = (el, color, x, y) => {
-  const random = Math.random();
+const rain = () => {
+  let timer = 0;
+  let interval;
+  let stop = false;
 
-  el.beginFill(color);
+  const changeTimer = () => {
+    stop = false;
 
-  switch (true) {
-    case (random < 0.142):
-      el.drawCircle(0, 0, Math.random() * 20 + 5);
-      el.x = x;
-      el.y = y;
-      surfaceSum += el.height * el.width * 0.6;
-      surface.innerHTML = surfaceSum.toFixed(0);
-
-      return;
-    case (random < 0.285):
-      el.drawCircle(0, 0, Math.random() * 25 + 10);
-      break;
-    case (random <= 0.428):
-      el.moveTo(25, -25);
-      el.lineTo(0, 25);
-      el.lineTo(-25, -25);
-      break;
-    case (random < 0.571):
-      el.drawRect(-25, -25, 50, 50);
-      break;
-    case (random < 0.714):
-      el.drawPolygon(
-        0, 25,
-        Math.sin(2 * Math.PI / 5) * 25, Math.cos(2 * Math.PI / 5) * 25,
-        Math.sin(4 * Math.PI / 5) * 25, -Math.cos(Math.PI / 5) * 25,
-        -Math.sin(4 * Math.PI / 5) * 25, -Math.cos(Math.PI / 5) * 25,
-        -Math.sin(2 * Math.PI / 5) * 25, Math.cos(2 * Math.PI / 5) * 25,
-      );
-      break;
-    case (random < 0.857):
-      el.drawPolygon(
-        0, 25,
-        Math.cos(Math.PI / 6) * 25, 12.5,
-        Math.cos(Math.PI / 6) * 25, -12.5,
-        0, -25,
-        -Math.cos(Math.PI / 6) * 25, -12.5,
-        -Math.cos(Math.PI / 6) * 25, 12.5,
-      );
-      break;
-    case (random <= 1):
-      shapeGenerate(el, color, x, y);
-      el.endFill();
-      surfaceSum -= el.height * el.width * 0.6;
-      surface.innerHTML = surfaceSum.toFixed(0);
-      el.beginFill(color);
-      shapeGenerate(el, color, x, y);
-      el.endFill();
-      surfaceSum -= el.height * el.width * 0.6;
-      surface.innerHTML = surfaceSum.toFixed(0);
-      el.beginFill(color);
-      shapeGenerate(el, color, x, y);
-      surfaceSum -= el.height * el.width * 0.6;
-      surface.innerHTML = surfaceSum.toFixed(0);
-
-      break;
+    timer = shapesControl.value > 0
+      ? 1000 / shapesControl.value
+      : stop = true;
   };
 
-  el.setTransform(
-    x,
-    y,
-    Math.random() + 0.5,
-    Math.random() + 0.5,
-    Math.random() * 360,
-    Math.random() - 0.5,
-    0,
-    0,
-    0,
-  );
-  el.endFill();
-  surfaceSum += el.height * el.width * 0.6;
-  surface.innerHTML = surfaceSum.toFixed(0);
-  el.interactive = true;
-  el.buttonMode = true;
-};
+  const intervalGenerator = () => {
+    clearInterval(interval);
 
-app.renderer.view.addEventListener('click', (e) => {
-  if (e.target.style.cursor !== 'inherit') {
-    return;
+    stop
+      ? changeTimer()
+      : makeShape(Math.random() * app.renderer.screen.width, -50);
+    changeTimer();
+    interval = setInterval(intervalGenerator, timer);
   };
 
-  makeShape(e.pageX - 25, e.pageY - 75);
-
-  // new Shape(e.pageX - 25, e.pageY - 50);
-
-  // newShape();
-});
-
-let t = 0;
-let interval;
-
-f1();
-
-let stop = false;
-
-function changeTimer() {
-  stop = false;
-
-  t = shapesButton.value > 0
-    ? 1000 / shapesButton.value
-    : stop = true;
+  intervalGenerator();
 };
 
-function f1() {
-  clearInterval(interval);
-
-  stop
-    ? changeTimer()
-    : makeShape(Math.random() * app.renderer.screen.width, -50);
-  changeTimer();
-  interval = setInterval(f1, t);
-};
+rain();
